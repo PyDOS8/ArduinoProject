00b0edeccedda357fce229bf89419363a7e19934
PyTerminal <124309800+PyTerminal@users.noreply.github.com>
2023-03-18 14:17:59 -0400

    Update DiceGame.ino

--- DiceGame.ino
+++ DiceGame.ino
@@ -8,10 +8,14 @@ void setup(){
   pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
 }
 void loop(){
   if(digitalRead(3)){
-    randNum = random(1,7);
+    randNum = random(1,20);
+    Serial.println(randNum);
     if(randNum == 1){
       digitalWrite(2, HIGH);
       delay(1000);
@@ -25,13 +29,44 @@ void loop(){
       digitalWrite(4, LOW);
     }
     if(randNum == 3){
-      Serial.println("You Win!");
       digitalWrite(2, HIGH);
       digitalWrite(4, HIGH);
       digitalWrite(5, HIGH);
-      digitalWtite(6, HIGH);
+      delay(1000);
+      digitalWrite(5, LOW);
+      digitalWrite(4, LOW);
+      digitalWrite(2, LOW);
+    }
+    if(randNum == 4){
+      digitalWrite(2, HIGH);
+      digitalWrite(4, HIGH);
+      digitalWrite(5, HIGH);
+      digitalWrite(6, HIGH);
+      delay(1000);
+      digitalWrite(6, LOW);
+      digitalWrite(5, LOW);
+      digitalWtite(4, LOW);
+      digitalWrite(2, LOW);
+    }
+    if(randNum == 5){
+      digitalWrite(2, HIGH);
+      digitlaWrite(4, HIGH);
+      digitalWrite(5, HIGH);
+      digitalWrite(6, HIGH);
       digitalWrite(7, HIGH);
-      digitalWrite(8, HIGH)
+      delay(1000);
+      digitalWrite(7, LOW);
+      digitalWrite(6, LOW);
+      digitalWrite(5, LOW);
+      digitalWrite(2, LOW);
+    }
+    if(randNum == 6){
+      digitalWrite(2, HIGH);
+      digitalWrite(4, HIGH);
+      digitalWrite(5, HIGH);
+      digitalWrite(6, HIGH);
+      digitalWrite(7, HIGH);
+      digitalWrite(8, HIGH);
       delay(1000);
       digitalWrite(8, LOW);
       digitalWrite(7, LOW);
@@ -40,23 +75,129 @@ void loop(){
       digitalWrite(4, LOW);
       digitalWrite(2, LOW);
     }
-    if(randNum == 4){
-      Serial.println("You Lose!");
+    if(randNum == 7){
       digitalWrite(2, HIGH);
       digitalWrite(4, HIGH);
       digitalWrite(5, HIGH);
       digitalWrite(6, HIGH);
       digitalWrite(7, HIGH);
-      digitalWtite(8, HIGH);
+      digitalWrite(8, HIGH);
       digitalWrite(9, HIGH);
-      delay(2000);
+      delay(1000);
       digitalWrite(9, LOW);
       digitalWrite(8, LOW);
       digitalWrite(7, LOW);
       digitalWrite(6, LOW);
       digitalWrite(5, LOW);
-      digitalWtite(4, LOW);
+      digitalWrite(4, LOW);
       digitalWrite(2, LOW);
+    }
+    if(randNum == 8){
+        digitalWrite(2, HIGH);
+        digitalWrite(4, HIGH);
+        digitalWrite(5, HIGH);
+        digitalWrite(6, HIGH);
+        digitalWrite(7, HIGH);
+        digitalWrite(8, HIGH);
+        digitalWrite(9, HIGH);
+        digitalWrite(10, HIGH);
+        delay(1000);
+        digitalWrite(10, LOW);
+        digitalWrite(9, LOW);
+        digitalWrite(8, LOW);
+        digitalWrite(7, LOW);
+        digitalWrite(6, LOW);
+        digitalWrite(5, LOW);
+        digitalWrite(4, LOW);
+        digitalWrite(2, LOW);
+    }
+    if(randNum == 9){
+      digitalWrite(2, HIGH);
+      digitalWrite(4, HIGH);
+      digitalWrite(5, HIGH);
+      digitalWrite(6, HIGH);
+      digitalWrite(7, HIGH);
+      digitalWrite(8, HIGH);
+      digitalWrite(9, HIGH);
+      digitalWrite(10, HIGH);
+      digitalWrite(11, HIGH);
+      delay(1000);
+      digitalWrite(11, LOW);
+      digitalWrite(10, LOW);
+      digitalWrite(9, LOW);
+      digitalWrite(8, LOW);
+      digitalWrite(7, LOW);
+      digitalWrite(6, LOW);
+      digitalWrite(5, LOW);
+      digitalWrite(4, LOW);
+      digitalWrite(2, LOW);
+    }
+    if(randNum == 10){
+      digitalWrite(2, HIGH);
+      digitalWrite(4, HIGH);
+      digitalWrite(5, HIGH);
+      digitalWrite(6, HIGH);
+      digitalWrite(7, HIGH);
+      digitalWrite(9, HIGH);
+      digitalWrite(10, HIGH);
+      digitalWrite(11, HIGH);
+      digitalWrite(12, HIGH);
+      delay(1000);
+      digitalWrite(12, LOW);
+      digitalWrite(11, LOW);
+      digitalWrite(10, LOW);
+      digitalWrite(9, LOW);
+      digitalWrite(8, LOW);
+      digitalWrite(7, LOW);
+      digitalWrite(6, LOW);
+      digitalWrite(5, LOW);
+      digitalWrite(4, LOW);
+      digitalWrite(2, LOW);
+    }
+    if(randNum == 11){
+      digitalWrite(4, HIGH);
+      delay(1000);
+      digitalWrite(4, LOW);
+    }
+    if(randNum == 12){
+      digitalWrite(5, HIGH);
+      delay(1000);
+      digitalWrite(5, LOW);
+    }
+    if(randNum == 13){
+      digitalWrite(6, HIGH);
+      delay(1000);
+      digitalWrite(6, LOW);
+    }
+    if(randNum == 14){
+      digitalWrite(7, HIGH);
+      delay(1000);
+      digitalWrite(7, LOW);
+    }
+    if(randNum == 15){
+      digitalWrite(8, HIGH);
+      delay(1000);
+      digitalWrite(8, LOW);
+    }
+    if(randNum == 16){
+      digitalWrite(9, HIGH);
+      delay(1000);
+      digitalWrite(9, LOW);
+    }
+    if(randLED == 17){
+      digitalWrite(10, HIGH);
+      delay(1000);
+      digitalWrite(10, LOW);
+    }
+    if(randLED == 18){
+      digitalWrite(11, HIGH);
+      delay(1000);
+      digitalWrite(11, LOW);
+    }
+    if(randLED == 19){
+      digitalWrite(12, HIGH);
+      delay(2000);
+      digitalWrite(12, LOW);
     }
   }
 }
