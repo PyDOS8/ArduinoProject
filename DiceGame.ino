ffaedc353610e39c9de90a3cb95deb1906f58667
PyTerminal <124309800+PyTerminal@users.noreply.github.com>
2023-03-17 22:39:25 -0400

    Update DiceGame.ino

--- DiceGame.ino
+++ DiceGame.ino
@@ -31,10 +31,8 @@ void loop(){
       digitalWrite(5, HIGH);
       digitalWtite(6, HIGH);
       digitalWrite(7, HIGH);
-      digitalWrite(8, HIGH);
-      digitalWrite(9, HIGH);
+      digitalWrite(8, HIGH)
       delay(1000);
-      digitalWrite(9, LOW);
       digitalWrite(8, LOW);
       digitalWrite(7, LOW);
       digitalWrite(6, LOW);
