123746608cab21eb94db0e51e3c8ec1c35b789cc
PyTerminal <124309800+PyTerminal@users.noreply.github.com>
2023-03-17 22:17:59 -0400

    Update DiceGame.ino

--- DiceGame.ino
+++ DiceGame.ino
@@ -4,12 +4,37 @@ void setup(){
   pinMod(3, OUTPUT);
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, INPUT);
+  randomSeed(analogRead(0));
 }
 void loop(){
-  randNum = random(1,5);
-  if(randNum == 1){
-     digitalWrite(2, HIGH);
-     delay(200);
-     digitalWrite(2, LOW);
+  if(digitalRead(7)){
+    randNum = random(1,5);
+    if(randNum == 1){
+      digitalWrite(2, HIGH);
+      delay(200);
+      digitalWrite(2, LOW);
     }
+    if(randNum == 2){
+      digitalWrite(3, HIGH);
+      delay(200);
+      digitalWrite(3, LOW);
+    }
+    if(randNum == 3){
+      digitalWrite(4, HIGH);
+      delay(200);
+      digitalWrite(i, LOW);
+  }
+  if(randNum == 4){
+    digitalWrite(5, HIGH);
+    delay(200);
+    digitalWrite(5,LOW);
+  }
+  if(randNum == 5){
+    digitalWrite(6, HIGH);
+    delay(200);
+    digitalWrite(6, LOW);
+   }
+  }
 }
